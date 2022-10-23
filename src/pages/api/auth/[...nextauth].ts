354785666679173213e252156795653d94bcd547
Anastasia <94125319+hovorun-nastya@users.noreply.github.com>
2022-10-23 16:01:55 +0200

    CU-3m39p1r - Set up deployment with github actions

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,8 +3,8 @@ import GoogleProvider from 'next-auth/providers/google';
 export default NextAuth ( {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string ,
       authorization: {
         params: {
           prompt: 'consent',
