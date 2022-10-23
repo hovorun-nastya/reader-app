685387e4e9cf7df0c26837a6b1099385f7e7c682
Anastasia <94125319+hovorun-nastya@users.noreply.github.com>
2022-10-23 15:07:30 +0200

    CU-3m39p1r - Set up deployment with github actions

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,6 +1,6 @@
 export const resolvers = {
   Query: {
-    users: (_parent, _arguments, context) => {
+    users: (_parent: any, _arguments: any, context: { prisma: { user: { findMany: () => any; }; }; }) => {
       return context.prisma.user.findMany();
     }
   },
