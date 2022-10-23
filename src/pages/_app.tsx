9422cf2838a1e6158c1cde6a1a9fc086aa917210
Anastasia <94125319+hovorun-nastya@users.noreply.github.com>
2022-10-23 15:18:41 +0200

    CU-3m39p1r - Set up deployment with github actions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,13 @@ import type { AppProps } from 'next/app';
 import {ApolloProvider} from '@apollo/client';
 import apolloClient from '../lib/apollo';
 import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
 
-function App({ Component, pageProps:{ session, ...pageProperties } }: AppProps) {
+function App({ Component, pageProps: { ...pageProperties }  }: AppProps<{
+  session: Session;
+}>) {
   return(
-    <SessionProvider session={ session }>
+    <SessionProvider session={ pageProperties.session }>
       <ApolloProvider client={ apolloClient }>
         <Component { ...pageProperties } />
       </ApolloProvider>
